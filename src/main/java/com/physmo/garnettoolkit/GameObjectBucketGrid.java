7162622d10a3bd9c5100a24afe5e2c1bd605f572
Nick Donnelly <nickd3000@hotmail.com>
2023-04-13 08:27:43 +0100

    Updates to collision system

--- src/main/java/com/physmo/garnettoolkit/GameObjectBucketGrid.java
+++ src/main/java/com/physmo/garnettoolkit/GameObjectBucketGrid.java
@@ -2,19 +2,18 @@ package com.physmo.garnettoolkit;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class GameObjectBucketGrid {
-    Map<Integer, Set<GameObject>> objects = new HashMap<>();
+    Map<Integer, List<Object>> objects = new HashMap<>();
     int cellWidth;
     int cellHeight;
 
     public GameObjectBucketGrid(int cellWidth, int cellHeight) {
         this.cellWidth = cellWidth;
         this.cellHeight = cellHeight;
+
     }
 
     public int getSize() {
@@ -25,31 +24,55 @@ public class GameObjectBucketGrid {
         objects = new HashMap<>();
     }
 
-    public void addObject(GameObject o) {
-        int num = num((int) (o.getTransform().x / cellWidth), (int) (o.getTransform().y / cellHeight));
+
+    public void addObject(Object o, int x, int y) {
+        int num = encoder(x / cellWidth, y / cellHeight);
 
         if (!objects.containsKey(num)) {
-            objects.put(num, new HashSet<>());
+            objects.put(num, new ArrayList<>());
         }
         objects.get(num).add(o);
     }
 
-    private int num(int x, int y) {
-        if (x < 3) x = 3;
-        if (y < 3) y = 3;
+    private int encoder(int x, int y) {
+        if (x < 0) x = 0;
+        if (y < 0) y = 0;
         if (x > 100) x = 100;
         if (y > 100) y = 100;
 
         return ((x & 0b1111_1111_1111) << 12) + (y & 0b1111_1111_1111);
     }
 
-    public List<GameObject> getObjects(int xPos, int yPos, int tileRadius) {
-        int cx = xPos / cellWidth;
-        int cy = yPos / cellHeight;
-        List<GameObject> list = new ArrayList<>();
-        for (int y = cy - tileRadius; y < cy + tileRadius; y++) {
-            for (int x = cx - tileRadius; x < cx + tileRadius; x++) {
-                int index = num(x, y);
+    public List<Integer[]> getListOfActiveCells() {
+        List<Integer[]> activeCells = new ArrayList<>();
+        for (Integer integer : objects.keySet()) {
+            activeCells.add(decoder(integer));
+        }
+        return activeCells;
+    }
+
+    private Integer[] decoder(int v) {
+        int x = (v >> 12) & 0b1111_1111_1111;
+        int y = (v) & 0b1111_1111_1111;
+        return new Integer[]{x, y};
+    }
+
+    public List<Object> getCellObjects(int cellX, int cellY) {
+        int index = encoder(cellX, cellY);
+        if (objects.containsKey(index)) {
+            return objects.get(index);
+        }
+
+        return new ArrayList<>();
+    }
+
+    public List<Object> getSurroundingObjects(int cellX, int cellY, int tileRadius) {
+        int cx = cellX;
+        int cy = cellY;
+        List<Object> list = new ArrayList<>();
+        for (int y = cy - tileRadius; y <= cy + tileRadius; y++) {
+            for (int x = cx - tileRadius; x <= cx + tileRadius; x++) {
+                int index = encoder(x, y);
                 if (objects.containsKey(index)) {
                     list.addAll(objects.get(index));
                 }
--- src/main/java/com/physmo/garnettoolkit/simplecollision/CollisionSystem.java
+++ src/main/java/com/physmo/garnettoolkit/simplecollision/CollisionSystem.java
@@ -2,6 +2,7 @@ package com.physmo.garnettoolkit.simplecollision;
 
 
 import com.physmo.garnettoolkit.GameObject;
+import com.physmo.garnettoolkit.GameObjectBucketGrid;
 import com.physmo.garnettoolkit.Rect;
 
 import java.util.ArrayList;
@@ -12,6 +13,7 @@ public class CollisionSystem extends GameObject {
     List<Collidable> collidables = new ArrayList<>();
     List<Collidable> collidablesPendingRemoval = new ArrayList<>();
     CollisionDrawingCallback collisionDrawingCallback = null;
+    int testsPerFrame = 0;
 
     public CollisionSystem(String name) {
         super(name);
@@ -23,46 +25,27 @@ public class CollisionSystem extends GameObject {
 
     @Override
     public void init() {
-        collidables = new ArrayList<>();
+
+    }
+
+    public int getTestsPerFrame() {
+        return testsPerFrame;
     }
 
     @Override
     public void tick(double t) {
+        testsPerFrame = 0;
         removePendingCollidables();
 
-        List<CollisionPacket> collisions = new ArrayList<>();
 
-        List<Collidable> activeCollidables = getListOfActiveCollidables();
-
-
-        for (Collidable c1 : activeCollidables) {
-            for (Collidable c2 : activeCollidables) {
-                if (c1 == c2) continue;
-                boolean collided = testCollision(c1, c2);
-                if (collided) collisions.add(new CollisionPacket(c1, c2));
-            }
-        }
+//        List<CollisionPacket> collisions = calculateCollisions();
+        List<CollisionPacket> collisions = calculateCollisions2();
 
         // Notify collider of collision.
         for (CollisionPacket collision : collisions) {
             collision.sourceEntity.collisionCallback(collision);
         }
 
-    }
-
-    private List<Collidable> getListOfActiveCollidables() {
-        List<Collidable> activeCollidables = new ArrayList<>();
-        for (Collidable collidable : collidables) {
-            if (!collidable.collisionGetGameObject().isActive()) continue;
-            activeCollidables.add(collidable);
-        }
-        return activeCollidables;
-    }
-
-    public boolean testCollision(Collidable c1, Collidable c2) {
-        Rect rect1 = c1.collisionGetRegion();
-        Rect rect2 = c2.collisionGetRegion();
-        return rect1.intersect(rect2);
     }
 
     private void removePendingCollidables() {
@@ -77,6 +60,68 @@ public class CollisionSystem extends GameObject {
 
         collidablesPendingRemoval.clear();
         collidables = keepList;
+    }
+
+    private List<CollisionPacket> calculateCollisions2() {
+        List<CollisionPacket> collisions = new ArrayList<>();
+        GameObjectBucketGrid bucketGrid = new GameObjectBucketGrid(32, 32);
+        for (Collidable collidable : getListOfActiveCollidables()) {
+            bucketGrid.addObject(collidable, (int) collidable.collisionGetRegion().x, (int) collidable.collisionGetRegion().y);
+        }
+
+
+        List<Integer[]> listOfActiveCells = bucketGrid.getListOfActiveCells();
+        for (Integer[] cellCoords : listOfActiveCells) {
+
+            List<Object> cellObjects = bucketGrid.getCellObjects(cellCoords[0], cellCoords[1]);
+            List<Object> surroundingObjects = bucketGrid.getSurroundingObjects(cellCoords[0], cellCoords[1], 1);
+
+            for (Object cellObject : cellObjects) {
+                for (Object surroundingObject : surroundingObjects) {
+                    if (cellObject == surroundingObject) continue;
+
+                    boolean collided = testCollision((Collidable) cellObject, (Collidable) surroundingObject);
+                    if (collided) collisions.add(
+                            new CollisionPacket((Collidable) cellObject, (Collidable) surroundingObject));
+                }
+
+            }
+
+        }
+
+
+        return collisions;
+    }
+
+    private List<Collidable> getListOfActiveCollidables() {
+        List<Collidable> activeCollidables = new ArrayList<>();
+        for (Collidable collidable : collidables) {
+            if (!collidable.collisionGetGameObject().isActive()) continue;
+            activeCollidables.add(collidable);
+        }
+        return activeCollidables;
+    }
+
+    public boolean testCollision(Collidable c1, Collidable c2) {
+        testsPerFrame++;
+        Rect rect1 = c1.collisionGetRegion();
+        Rect rect2 = c2.collisionGetRegion();
+        return rect1.intersect(rect2);
+    }
+
+    private List<CollisionPacket> calculateCollisions() {
+        List<CollisionPacket> collisions = new ArrayList<>();
+        List<Collidable> activeCollidables = getListOfActiveCollidables();
+
+        for (Collidable c1 : activeCollidables) {
+            for (Collidable c2 : activeCollidables) {
+                if (c1 == c2) continue;
+                boolean collided = testCollision(c1, c2);
+                if (collided) collisions.add(new CollisionPacket(c1, c2));
+            }
+        }
+
+        return collisions;
     }
 
     @Override
